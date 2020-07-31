bfca2d8c5560a80722c969dcedf6bb5438e5ddba
Hamid Molareza <kitm.hamid@gmail.com>
2020-07-31 22:01:17 +0430

    TDD: MethodParametersMustValid_NoParameter_Success

--- ModelsValidation/ModelsValidation.cs
+++ ModelsValidation/ModelsValidation.cs
@@ -3,30 +3,65 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
-using ModelsValidation.ResultDetails;
 using FunctionalUtility.Extensions;
 using FunctionalUtility.ResultDetails;
 using FunctionalUtility.ResultUtility;
 using Microsoft.AspNetCore.Http;
+using ModelsValidation.ResultDetails;
 
 namespace ModelsValidation {
     public static class Validation {
-        private static MethodResult<ParameterInfo[]> ValidateInputs (
+        //TODO: 1
+        public static MethodResult<MethodBase> MethodParametersMustValid (
+                this MethodBase methodBase,
+                IReadOnlyCollection<object?> ? values) =>
+            InputsMustValid (methodBase, values)
+            .OnSuccessOperateWhen (parameters => parameters.IsNotNullOrEmpty (),
+                parameters =>
+                MethodParametersMustValid (parameters, values)
+                .MapMethodResult (parameters)
+            )
+            .MapMethodResult (methodBase);
+
+        //TODO: 2
+        private static MethodResult<ParameterInfo[]> InputsMustValid (
                 MethodBase methodBase,
-                IReadOnlyCollection<object?> ? values) => methodBase
-            .IsNotNull<MethodBase> (new ErrorDetail (StatusCodes.Status400BadRequest,
+                IReadOnlyCollection<object?> ? values) =>
+            methodBase.IsNotNull<MethodBase> (new ErrorDetail (StatusCodes.Status400BadRequest,
                 $"{nameof(methodBase)} is required."))
             .TryOnSuccess (methodBase.GetParameters)
             .OnSuccessFailWhen (parameters =>
-                parameters.IsNullOrEmpty () && !values.IsNullOrEmpty (), new ArgumentError (
+                parameters.IsNullOrEmpty () && !values.IsNullOrEmpty (), new ErrorDetail (
+                    StatusCodes.Status400BadRequest,
                     message: "The method parameters are inconsistent with the given input."))
             .OnSuccessFailWhen (parameters =>
-                !parameters.IsNullOrEmpty () && (values.IsNullOrEmpty () || parameters.Length != values!.Count),
-                new ArgumentError (message: "The method parameters are inconsistent with the given input."));
+                !parameters.IsNullOrEmpty () &&
+                (values.IsNullOrEmpty () || parameters.Length != values!.Count),
+                new ErrorDetail (StatusCodes.Status400BadRequest,
+                    message: "The method parameters are inconsistent with the given input."));
 
+        //TODO: 3
+        private static MethodResult MethodParametersMustValid (
+                IReadOnlyCollection<ParameterInfo> parameters,
+                IReadOnlyCollection<object?> ? values
+            ) => MapParametersAndValues (parameters, values)
+            .OnSuccess (parametersWithValues =>
+                (validationContext: new ValidationContext (values), parametersWithValues))
+            .OnSuccess (result =>
+                result.parametersWithValues.ForEachUntilIsSuccess (parameterWithValue =>
+                    ModelPropertiesMustValid (
+                        parameterWithValue.Key.GetCustomAttributesData ().ToList (),
+                        parameterWithValue.Key.Name,
+                        parameterWithValue.Value, result.validationContext)
+                )
+            );
+
+        //TODO: 4
+        //TODO: ===
         private static MethodResult<List<KeyValuePair<ParameterInfo, object>>> MapParametersAndValues (
-            IReadOnlyCollection<ParameterInfo> parameters, IReadOnlyCollection<object?> values) {
+            IReadOnlyCollection<ParameterInfo> parameters, IReadOnlyCollection<object?> ? values) {
             var result = new List<KeyValuePair<ParameterInfo, object>> (parameters.Count);
+
             for (var i = 0; i < parameters.Count; i++) {
                 var value = values.ElementAt (i);
                 var parameter = parameters.ElementAt (i);
@@ -35,65 +70,68 @@ namespace ModelsValidation {
                         new ArgumentError (message: "input types are inconsistent with parameters type."));
                 }
 
-                result.Add (new KeyValuePair<ParameterInfo, object> (parameter, values));
+                result.Add (new KeyValuePair<ParameterInfo, object> (parameter, value));
             }
 
             return MethodResult<List<KeyValuePair<ParameterInfo, object>>>.Ok (result);
         }
 
-        private static MethodResult ValidateProperties (
+        //TODO: 5
+        private static MethodResult ModelPropertiesMustValid (
                 IReadOnlyCollection<CustomAttributeData> attributes,
                 string? propertyName,
                 object? value,
                 ValidationContext validationContext) =>
             OperateExtensions.OperateWhen (attributes.Any (),
                 () => PropertyMustValid (attributes, propertyName, value, validationContext))
-            .OnSuccessOperateWhen (value != null,
-                () => ModelPropertiesMustValid (value).MapMethodResult ());
+            .OnSuccessOperateWhen (value != null && value.GetType ().Namespace != "System",
+                () => ModelPropertiesMustValid (value)
+                .MapMethodResult ());
 
-        private static MethodResult MethodParametersMustValid (
-                IReadOnlyCollection<ParameterInfo> parameters,
-                IReadOnlyCollection<object?> values
-            ) => MapParametersAndValues (parameters, values)
-            .OnSuccess (parametersWithValues =>
-                (validationContext: new ValidationContext (values), parametersWithValues))
-            .OnSuccess (result =>
-                result.parametersWithValues.ForEachUntilIsSuccess (parameterWithValue =>
-                    ValidateProperties (
-                        parameterWithValue.Key.CustomAttributes as IReadOnlyCollection<CustomAttributeData>, parameterWithValue.Key.Name,
-                        parameterWithValue.Value, result.validationContext)
+        //TODO: 6
+        private static MethodResult PropertyMustValid (
+                IReadOnlyCollection<CustomAttributeData> attributes,
+                string? propertyName,
+                object? value,
+                ValidationContext? instance = null) =>
+            OperateExtensions.OperateWhen (attributes.Any (),
+                () => GetValidParameterName (propertyName, instance)
+                .IsNotNull<string> (new ErrorDetail (StatusCodes.Status400BadRequest,
+                    "Can not detect parameter name."))
+                .OnSuccess (validParameterName =>
+                    attributes.ForEachUntilIsSuccess (attribute =>
+                        ValidateByAttribute (attribute, validParameterName, value, instance)
+                    )
                 )
             );
 
-        public static MethodResult<MethodBase> MethodParametersMustValid (
-                this MethodBase methodBase,
-                IReadOnlyCollection<object?> ? values) =>
-            ValidateInputs (methodBase, values)
-            .OnSuccessOperateWhen (parameters => !parameters.IsNullOrEmpty (),
-                parameters =>
-                MethodParametersMustValid (parameters, values).MapMethodResult (parameters)
-            ).MapMethodResult (methodBase);
+        //TODO: 7
+        public static MethodResult<T> ModelPropertiesMustValid<T> (
+                this T model) => MethodResult<T>.Ok (model)
+            .OnSuccessOperateWhen (model.IsNotNull<T> ().IsSuccess,
+                () => model!.GetType ().GetProperties ()
+                .OperateWhen (propertyInfos => propertyInfos.Any (),
+                    propertyInfos => propertyInfos.ModelPropertiesMustValid (model)
+                    .MapMethodResult (propertyInfos))
+                .MapMethodResult (model)
+            );
+
+        //TODO: 8
+        private static MethodResult ModelPropertiesMustValid<T> (
+                this IEnumerable<PropertyInfo> propertyInfos, T model) =>
+            propertyInfos.ForEachUntilIsSuccess (
+                propertyInfo => ModelPropertiesMustValid (
+                    propertyInfo.GetCustomAttributesData ().ToList (),
+                    propertyInfo.Name, propertyInfo.TryGetValue (model), new ValidationContext (model))
+            );
 
         public static MethodResult<T> ModelsPropertiesMustValid<T> (
-                this T models) where T : IEnumerable<object> => models
-            .ForEachUntilIsSuccess (model => ModelPropertiesMustValid (model).MapMethodResult ())
+                this T models) where T : IEnumerable<object> =>
+            models.ForEachUntilIsSuccess (model => ModelPropertiesMustValid (model).MapMethodResult ())
             .MapMethodResult (models);
 
-        public static MethodResult<T> ModelPropertiesMustValid<T> (
-                this T model) =>
-            model.IsNotNull<T> ()
-            .TryOnSuccess (() => model.GetType ().GetProperties ())
-            .OnSuccessOperateWhen (propertyInfos => propertyInfos.Any (),
-                propertyInfos =>
-                propertyInfos.ForEachUntilIsSuccess (propertyInfo =>
-                    ValidateProperties (
-                        propertyInfo.CustomAttributes as IReadOnlyCollection<CustomAttributeData>,
-                        propertyInfo.Name, propertyInfo.TryGetValue (model), new ValidationContext (model))
-                ).MapMethodResult (propertyInfos)
-            ).MapMethodResult (model);
-
         private static string? GetValidParameterName (
-                string propertyName,
+                string? propertyName,
                 ValidationContext? instance) =>
             !string.IsNullOrEmpty (propertyName) ? propertyName :
             instance is null || string.IsNullOrEmpty (instance.DisplayName) ? null :
@@ -126,25 +164,10 @@ namespace ModelsValidation {
                     .GetType ()
                     .GetProperty (item.MemberName, BindingFlags.Public | BindingFlags.Instance)
                     .OperateWhen (property => property != null && property.CanWrite,
-                        property => TryExtensions.Try (() => property!.SetValue (classObj, item.TypedValue.Value, null))
-                    ).MapMethodResult ()
+                        property => TryExtensions.Try (() => property!.SetValue (classObj,
+                            item.TypedValue.Value, null)))
+                    .MapMethodResult ()
                 ).MapMethodResult (classObj)
-            );
-
-        private static MethodResult PropertyMustValid (
-                IReadOnlyCollection<CustomAttributeData> attributes,
-                string? propertyName,
-                object? value,
-                ValidationContext? instance = null) =>
-            OperateExtensions.OperateWhen (attributes.Any (),
-                () => GetValidParameterName (propertyName, instance)
-                .IsNotNull<string> (new ErrorDetail (StatusCodes.Status400BadRequest,
-                    "Can not detect parameter name."))
-                .OnSuccess (validParameterName =>
-                    attributes.ForEachUntilIsSuccess (attribute =>
-                        ValidateByAttribute (attribute, validParameterName, value, instance)
-                    )
-                )
             );
 
         //TODO: Refactor
@@ -156,44 +179,52 @@ namespace ModelsValidation {
 
             switch (validationResult) {
                 case bool isValid:
-                    if (!isValid) {
-                        var errorMessageMethod = attributeType.GetMethod ("FormatErrorMessage");
-                        if (errorMessageMethod is null)
-                            return MethodResult.Fail (
-                                new ArgumentError (message: "Can not find (FormatErrorMessage) to get error."));
-
-                        var errorMessage = errorMessageMethod.Invoke (classObject, new object[] { parameterName });
-                        if (errorMessage != null) {
-                            if (!(errorMessage is string errorResult))
+                    {
+                        if (!isValid) {
+                            var errorMessageMethod = attributeType.GetMethod ("FormatErrorMessage");
+                            if (errorMessageMethod is null)
                                 return MethodResult.Fail (
-                                    new ArgumentError (message: "Type of error message is not expected."));
-                            return MethodResult.Fail (new ArgumentValidationError (new List<string> { errorResult }));
+                                    new ArgumentError (message: "Can not find (FormatErrorMessage) to get error."));
+
+                            var errorMessage = errorMessageMethod.Invoke (classObject, new object[] { parameterName });
+                            if (errorMessage != null) {
+                                if (!(errorMessage is string errorResult))
+                                    return MethodResult.Fail (
+                                        new ArgumentError (message: "Type of error message is not expected."));
+                                return MethodResult.Fail (new ArgumentValidationError (new List<string> { errorResult }));
+                            }
                         }
+
+                        break;
                     }
-                    break;
                 case ValidationResult result:
-                    if (!string.IsNullOrEmpty (result.ErrorMessage))
-                        return MethodResult.Fail (new ArgumentValidationError (new List<string> { result.ErrorMessage }));
-                    break;
+                    {
+                        if (!string.IsNullOrEmpty (result.ErrorMessage))
+                            return MethodResult.Fail (new ArgumentValidationError (new List<string> { result.ErrorMessage }));
+                        break;
+                    }
                 default:
-                    return MethodResult.Fail (new ArgumentError (
-                        title: "OutOfRangeError", message: $"Type of {nameof(validationResult)} is not expected."));
+                    {
+                        return MethodResult.Fail (new ErrorDetail (StatusCodes.Status500InternalServerError,
+                            title: "OutOfRangeError", message: $"Type of {nameof(validationResult)} is not expected." +
+                            $" ({typeof(ValidationResult)})"));
+                    }
             }
 
             return MethodResult.Ok ();
         }
 
+        //TODO: Here - 1
         private static MethodResult ValidateByAttribute (
                 CustomAttributeData attribute, string parameterName,
-                object value, ValidationContext validationContext) =>
-            OperateExtensions.OperateWhen (attribute.AttributeType == typeof (ValidationAttribute),
+                object? value, ValidationContext? validationContext) =>
+            OperateExtensions.OperateWhen (attribute.AttributeType.IsSubclassOf (typeof (ValidationAttribute)),
                 () => CreateAttributeObject (attribute)
-                .OnSuccess (attributeObj =>
-                    (validationResult: ValidateByAttribute (attribute, attributeObj, parameterName, value,
-                        validationContext), attributeObj))
+                .OnSuccess (attributeObj => ValidateByAttribute (attribute, attributeObj, parameterName, value,
+                    validationContext).OnSuccess (validationResult => (attribute, validationResult)))
                 .OnSuccess (result =>
                     ValidationResultIsSuccess (result.validationResult, attribute.AttributeType,
-                        result.attributeObj, parameterName))
+                        result.attribute, parameterName))
             );
 
         //TODO: Refactor
@@ -221,13 +252,12 @@ namespace ModelsValidation {
                                 new ArgumentError (
                                     message: $"Can not find validation method. ({attribute.AttributeType})"));
 
-                        var parameters = value is null ? new object[] { } : new [] { value };
-                        validationResult = validationMethod.Invoke (classObj, parameters);
+                        //TODO: Check parameters
+                        //var parameters = value is null ? new object[] { } : new [] { value };
+                        validationResult = validationMethod.Invoke (classObj, new [] { value });
                     } else {
-                        var parameters = value is null ?
-                            new object[] { validationContext } :
-                            new [] { value, validationContext };
-                        validationResult = validationMethod.Invoke (classObj, parameters);
+                        validationContext.DisplayName = parameterName;
+                        validationResult = validationMethod.Invoke (classObj, new [] { value, validationContext });
                     }
                 }
 
