f064f48fc333c7240e937cdf30bedc230135a03a
Hamid Molareza <kitm.hamid@gmail.com>
2020-08-01 21:46:54 +0430

    TDD: MethodParametersMustValid_NoAttribute_Success

---
+++ TestModelsValidation/TestMethod.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Reflection;
+using FunctionalUtility.Extensions;
+using FunctionalUtility.ResultUtility;
+using ModelsValidation;
+using Xunit;
+
+namespace TestModelsValidation {
+    public class TestMethod {
+        [Fact]
+        public void MethodParametersMustValid_NoParameter_Success () {
+            var result = NoParameter ();
+            Assert.True (result.IsSuccess);
+        }
+
+        [Fact]
+        public void MethodParametersMustValid_NoAttribute_Success () {
+            var result = NoAttribute (null, null, null, null, null);
+            Assert.True (result.IsSuccess);
+        }
+
+        private static MethodResult NoParameter () =>
+            MethodBase.GetCurrentMethod ()
+            .Map (currentMethod =>
+                currentMethod!.MethodParametersMustValid (new object[] { }))
+            .MapMethodResult ();
+
+        private static MethodResult NoAttribute (string a, int? b,
+                IReadOnlyCollection<char> c, List<SimpleModel> d, SimpleModel e) =>
+            MethodBase.GetCurrentMethod ()
+            .Map (currentMethod =>
+                currentMethod!.MethodParametersMustValid (
+                    new object[] { a, b, c, d, e }))
+            .MapMethodResult ();
+    }
+
+    public abstract class SimpleModel {
+        public string A { get; set; }
+        public int B { get; set; }
+        public IReadOnlyCollection<char> C { get; set; }
+        public List<int> D { get; set; }
+    }
+}
