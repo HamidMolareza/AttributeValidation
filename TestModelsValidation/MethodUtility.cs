888ea7c4c29c333cd35589894f5d0dbf4ad30775
Hamid Molareza <kitm.hamid@gmail.com>
2020-08-01 23:18:00 +0430

    Classify tests.

---
+++ TestModelsValidation/MethodUtility.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using FunctionalUtility.Extensions;
+using FunctionalUtility.ResultUtility;
+using ModelsValidation;
+
+namespace TestModelsValidation {
+    public static class MethodUtility {
+        public static MethodResult NoParameter () =>
+            MethodBase.GetCurrentMethod ()
+            .Map (currentMethod =>
+                currentMethod!.MethodParametersMustValid (new object[] { }))
+            .MapMethodResult ();
+
+        public static MethodResult NoAttribute (string a, int? b,
+                IReadOnlyCollection<char> c, List<SimpleModel> d, SimpleModel e) =>
+            MethodBase.GetCurrentMethod ()
+            .Map (currentMethod =>
+                currentMethod!.MethodParametersMustValid (
+                    new object[] { a, b, c, d, e }))
+            .MapMethodResult ();
+
+        public static MethodResult WrongImplementation (string a, int? b,
+                IReadOnlyCollection<char> c, List<SimpleModel> d, SimpleModel e) =>
+            MethodBase.GetCurrentMethod ()
+            .Map (currentMethod =>
+                currentMethod!.MethodParametersMustValid (
+                    new object[] { a, b, c, d }))
+            .MapMethodResult ();
+
+        public static MethodResult ParametersWithAttributes (
+                [Required][StringLength (3)] string a, [Required][Range (0, 10)] int? b, [Required] List<int> c) =>
+            MethodBase.GetCurrentMethod ()
+            .Map (currentMethod =>
+                currentMethod!.MethodParametersMustValid (
+                    new object[] { a, b, c }))
+            .MapMethodResult ();
+
+        public static MethodResult ModelParameters (
+                ModelWithAttributes a, [Required] ModelWithAttributes b,
+                ComplexModel c, [Required] ComplexModel d, string e) =>
+            MethodBase.GetCurrentMethod ()
+            .Map (currentMethod =>
+                currentMethod!.MethodParametersMustValid (
+                    new object[] { a, b, c, d, e }))
+            .MapMethodResult ();
+    }
+}
--- TestModelsValidation/TestMethod.cs
+++ TestModelsValidation/TestMethod.cs
@@ -1,9 +1,4 @@
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
-using FunctionalUtility.Extensions;
-using FunctionalUtility.ResultUtility;
-using ModelsValidation;
 using ModelsValidation.ResultDetails;
 using Xunit;
 
@@ -11,32 +6,32 @@ namespace TestModelsValidation {
     public class TestMethod {
         [Fact]
         public void MethodParametersMustValid_NoParameter_Success () {
-            var result = TestMethods.NoParameter ();
+            var result = MethodUtility.NoParameter ();
             Assert.True (result.IsSuccess);
         }
 
         [Fact]
         public void MethodParametersMustValid_NoAttribute_Success () {
-            var result = TestMethods.NoAttribute (null, null, null, null, null);
+            var result = MethodUtility.NoAttribute (null, null, null, null, null);
             Assert.True (result.IsSuccess);
         }
 
         [Fact]
         public void MethodParametersMustValid_WrongImplementation_Error () {
-            var result = TestMethods.WrongImplementation (null, null, null, null, null);
+            var result = MethodUtility.WrongImplementation (null, null, null, null, null);
             Assert.False (result.IsSuccess);
             Assert.True (result.Detail is ArgumentError);
         }
 
         [Fact]
         public void MethodParametersMustValid_CorrectParametersWithAttributes_Success () {
-            var result = TestMethods.ParametersWithAttributes ("a", 5, new List<int> ());
+            var result = MethodUtility.ParametersWithAttributes ("a", 5, new List<int> ());
             Assert.True (result.IsSuccess);
         }
 
         [Fact]
         public void MethodParametersMustValid_WrongParametersWithAttributes_Error () {
-            var result = TestMethods.ParametersWithAttributes ("long string.", 5, null);
+            var result = MethodUtility.ParametersWithAttributes ("long string.", 5, null);
             Assert.False (result.IsSuccess);
         }
 
@@ -57,7 +52,7 @@ namespace TestModelsValidation {
                 ListOfModelWithAttributes = new List<ModelWithAttributes> { modelWithAttributes }
                 }
             };
-            var result = TestMethods.ModelParameters (null, modelWithAttributes, complexModel, complexModel, "");
+            var result = MethodUtility.ModelParameters (null, modelWithAttributes, complexModel, complexModel, "");
             Assert.True (result.IsSuccess);
         }
 
@@ -78,81 +73,9 @@ namespace TestModelsValidation {
                 ListOfModelWithAttributes = new List<ModelWithAttributes> { modelWithAttributes }
                 }
             };
-            var result = TestMethods.ModelParameters (null, modelWithAttributes, complexModel, complexModel, "");
+            var result = MethodUtility.ModelParameters (null, modelWithAttributes, complexModel, complexModel, "");
             Assert.False (result.IsSuccess);
             Assert.True (result.Detail is ArgumentValidationError);
-        }
-    }
-
-    public static class TestMethods {
-        public static MethodResult NoParameter () =>
-            MethodBase.GetCurrentMethod ()
-            .Map (currentMethod =>
-                currentMethod!.MethodParametersMustValid (new object[] { }))
-            .MapMethodResult ();
-
-        public static MethodResult NoAttribute (string a, int? b,
-                IReadOnlyCollection<char> c, List<SimpleModel> d, SimpleModel e) =>
-            MethodBase.GetCurrentMethod ()
-            .Map (currentMethod =>
-                currentMethod!.MethodParametersMustValid (
-                    new object[] { a, b, c, d, e }))
-            .MapMethodResult ();
-
-        public static MethodResult WrongImplementation (string a, int? b,
-                IReadOnlyCollection<char> c, List<SimpleModel> d, SimpleModel e) =>
-            MethodBase.GetCurrentMethod ()
-            .Map (currentMethod =>
-                currentMethod!.MethodParametersMustValid (
-                    new object[] { a, b, c, d }))
-            .MapMethodResult ();
-
-        public static MethodResult ParametersWithAttributes (
-                [Required][StringLength (3)] string a, [Required][Range (0, 10)] int? b, [Required] List<int> c) =>
-            MethodBase.GetCurrentMethod ()
-            .Map (currentMethod =>
-                currentMethod!.MethodParametersMustValid (
-                    new object[] { a, b, c }))
-            .MapMethodResult ();
-
-        public static MethodResult ModelParameters (
-                ModelWithAttributes a, [Required] ModelWithAttributes b,
-                ComplexModel c, [Required] ComplexModel d, string e) =>
-            MethodBase.GetCurrentMethod ()
-            .Map (currentMethod =>
-                currentMethod!.MethodParametersMustValid (
-                    new object[] { a, b, c, d, e }))
-            .MapMethodResult ();
-    }
-
-    public class SimpleModel {
-        public string A { get; set; }
-        public int B { get; set; }
-        public IReadOnlyCollection<char> C { get; set; }
-        public List<int> D { get; set; }
-    }
-
-    public class ModelWithAttributes {
-        [Required][StringLength (5)] public string A { get; set; }
-
-        [Range (0, 10)] public int? B { get; set; }
-        public IReadOnlyCollection<char> C { get; set; }
-
-        [Required][Range (0, 10)] public int? D { get; set; }
-    }
-
-    public class ComplexModel {
-        [Required] public SimpleModel SimpleModel { get; set; }
-
-        [Required] public List<ModelWithAttributes> ModelWithAttributes { get; set; }
-        public InnerClass InnerClassProp { get; set; }
-
-        public class InnerClass {
-            [Required] public SimpleModel SimpleModel { get; set; }
-
-            [Required] public ModelWithAttributes ModelWithAttributes { get; set; }
-
-            [Required] public List<ModelWithAttributes> ListOfModelWithAttributes { get; set; }
         }
     }
 }
